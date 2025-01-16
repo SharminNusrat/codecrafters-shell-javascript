2ecf4747f482fa12848dda7e17150aa3bbdaea1c
SharminNusrat <bsse1441@iit.du.ac.bd>
2025-01-16 17:04:21 +0600

    stage5

--- app/main.js
+++ app/main.js
@@ -7,12 +7,17 @@ const rl = readline.createInterface({
 
 function askQuestion() {
   rl.question("$ ", (answer) => {
-    // if(answer === `exit 0`) {
-    //   rl.close();
-    //   return;
-    // };
-    let result = answer.slice(5);
-    console.log(result);
+    if (answer === 'exit 0') {
+      rl.close();
+      return;
+    };
+    if (!answer.startsWith('echo ')) {
+      console.log(`${answer}: command not found`);
+    }
+    else {
+      let result = answer.slice(5);
+      console.log(result);
+    }
     askQuestion();
   });
 };
