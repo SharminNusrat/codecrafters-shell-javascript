364b77c51cedd402e1c0a001ecd5783bc2b65c90
SharminNusrat <bsse1441@iit.du.ac.bd>
2025-01-17 23:46:02 +0600

    stage6

--- app/main.js
+++ app/main.js
@@ -5,22 +5,41 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-function askQuestion() {
-  rl.question("$ ", (answer) => {
+const supportedTypes = ['type', 'echo', 'exit'];
+
+const handleEcho = (answer) => {
+  let result = answer.slice(5);
+  console.log(result);
+}
+
+const handleType = (answer) => {
+  let builtin = answer.slice(5);
+  if (supportedTypes.indexOf(builtin) > -1) {
+    console.log(`${builtin} is a shell builtin`);
+  }
+  else {
+    console.log(`${builtin}: not found`)
+  }
+}
+
+const main = () => {
+  rl.question('$ ', (answer) => {
     if (answer === 'exit 0') {
       rl.close();
       return;
-    };
-    if (!answer.startsWith('echo ')) {
-      console.log(`${answer}: command not found`);
+    }
+
+    if (answer.startsWith('echo ')) {
+      handleEcho(answer);
+    }
+    else if (answer.startsWith('type ')) {
+      handleType(answer);
     }
     else {
-      let result = answer.slice(5);
-      console.log(result);
+      console.log(`${answer}: command not found`);
     }
-    askQuestion();
+    main();
   });
-};
+}
 
-
-askQuestion();
+main();
