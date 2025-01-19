fe06c8b65f375a4aacdbabc58ee842f1c04669aa
SharminNusrat <bsse1441@iit.du.ac.bd>
2025-01-20 01:01:41 +0600

    stage7

--- app/main.js
+++ app/main.js
@@ -1,4 +1,6 @@
 const readline = require("readline");
+const fs = require("fs");
+const path = require("path");
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -13,16 +15,33 @@ const handleEcho = (answer) => {
 }
 
 const handleType = (answer) => {
-  let builtin = answer.slice(5);
-  if (supportedTypes.indexOf(builtin) > -1) {
-    console.log(`${builtin} is a shell builtin`);
+  let command = answer.slice(5);
+
+  // console.log(process.env.PATH);
+  if (supportedTypes.indexOf(command) > -1) {
+    console.log(`${command} is a shell builtin`);
+    return;
   }
-  else {
-    console.log(`${builtin}: not found`)
+
+  const pathDirs = process.env.PATH.split(";");
+  for(const dir of pathDirs) {
+    const filePath = path.join(dir, command);
+
+    if(fs.existsSync(filePath)) {
+      try {
+        fs.accessSync(filePath, fs.constants.X_OK);
+        console.log(`${command} is ${filePath}`);
+        return;
+      } catch {
+        continue;
+      }
+    }
   }
+  console.log(`${command}: not found`);
 }
 
 const main = () => {
+  // console.log(process.argv);
   rl.question('$ ', (answer) => {
     if (answer === 'exit 0') {
       rl.close();
